6b60d37303cb644675f2980c605b8b05bac44e97
Dan Matthews <dan@bluefoc.us>
2013-10-27 21:36:46 -0400

    Adding Tweet creation

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,28 @@ class TweetsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    get_tweets
+  end
+
+  def create
+    @tweet = current_user.tweets.create(tweet_params)
+    if @tweet.valid?
+      flash[:success] = "Your tweet was shared"
+      redirect_to :action => :index and return
+    else
+      get_tweets
+      flash[:error] = "Your tweet could not be saved"
+      render :action => :index and return
+    end
+  end
+
+  private
+
+  def get_tweets
     @tweets = current_user.all_tweets
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
   end
 end
--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -9,6 +9,13 @@ describe TweetsController do
 
       it { should_not be_successful }
     end
+
+    describe "POST create" do
+      subject { response }
+      before { post :create, :tweet => { :content => "Hello World!" } }
+
+      it { should_not be_successful }
+    end
   end
 
   context "when a user is logged in" do
@@ -25,6 +32,26 @@ describe TweetsController do
 
       it "should assign @tweets" do
         assigns[:tweets].should_not be_nil
+      end
+    end
+
+    describe "POST create" do
+      let(:following) { FactoryGirl.create(:user) }
+
+      before { post :create, :tweet => { :content => "Hello World!" } }
+
+      it "should redirect to GET index" do
+        response.should redirect_to tweets_path
+      end
+
+      it "should display a success message" do
+        flash[:success].should == "Your tweet was shared"
+      end
+
+      it "should create a new record" do
+        expect {
+          post :create, :tweet => { :content => "Hello World!" }
+        }.to change { Tweet.count }.by(1)
       end
     end
   end
